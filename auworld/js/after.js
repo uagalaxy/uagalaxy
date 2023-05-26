f6d60d22fa1e49c5fa0011bc141eb6f6e4dbd9ec
maakasamindian <74304156+maakasamindian@users.noreply.github.com>
2023-05-26 13:34:19 +0530

    Update after.js

--- auworld/js/after.js
+++ auworld/js/after.js
@@ -58,7 +58,7 @@ postMenu.forEach((menu) => {
 
 
 function cardMenu(au) {
-  const card = au.parentElement.parentElement;
+  const card = au.parentElement;
   let linkName = card.querySelector(".au-card-img").getAttribute("title");
   let linkUrl =  card.querySelector(".au-card-img").getAttribute("src");
 
