68fa81713bc8d546bb85bf581b6365f266cfa680
maakasamindian <74304156+maakasamindian@users.noreply.github.com>
2023-06-08 22:24:07 +0530

    Update after.js

--- auworld/js/after.js
+++ auworld/js/after.js
@@ -56,7 +56,7 @@ postMenu.forEach((menu) => {
   menu.setAttribute("onclick", "cardMenu(this)");
 });
 
-await function hideABC(l){
+ function hideABC(l){
  document.querySelector("#post").addEventListener("click", function (event) {
 	if(contextMenu.classList.contains("menu-open")){
 	     contextMenu.classList.remove("menu-open");
@@ -66,11 +66,11 @@ await function hideABC(l){
    
 });
 }
-function cardMenu(au) {
+async function cardMenu(au) {
   const card = au.parentElement.parentElement;
   let linkName = card.querySelector(".au-card-img").getAttribute("title");
   let linkUrl =  card.querySelector(".au-card-img").getAttribute("src");
-  document.querySelector("#post").setAttribute("onclick", "hideABC()");
+await hideABC();
   document.getElementById("share").setAttribute("data-name", linkName);
   document.getElementById("share").setAttribute("data-url", linkUrl);
   document.getElementById("copy").setAttribute("data-text", linkUrl);
